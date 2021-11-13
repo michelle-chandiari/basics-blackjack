106793bdb16889b1b318559c95878930e10627a6
michelle-chandiari <michellechandiari@gmail.com>
2021-11-13 11:14:34 +0800

    complete all versions of base and added a celebratory gif if player wins by black jack

--- script.js
+++ script.js
@@ -1,66 +1,221 @@
+var gameMode = `draw card`;
+var playerCard1 = ``;
+var playerCard2 = ``;
+var playerCardHit = ``;
+var computerCard1 = ``;
+var computerCard2 = ``;
+var computerCardHit = ``;
+var message = ``;
+var deck = [];
+var shuffledDeck = [];
+
 var main = function (input) {
-  var deck = makeDeck();
-  // console.log(`original deck ${deck}`);
+  // Check if there is an existing deck
+  if (shuffledDeck.length == 0) {
+    deck = makeDeck();
+    shuffledDeck = [...deck];
+    shuffledDeck = shuffle(shuffledDeck);
+  }
+  // User clicks Submit button to deal cards
+  if (gameMode == `draw card`) {
+    playerCard1 = drawCard();
+    playerCard2 = drawCard();
+    computerCard1 = drawCard();
+    computerCard2 = drawCard();
 
-  // Deck is shuffled
-  var shuffledDeck = shuffle(deck);
-  // console.log(shuffledDeck);
-
-  var playerCard1 = shuffledDeck.pop();
-  var playerCard2 = shuffledDeck.pop();
-  console.log(
-    `player cards: ${playerCard1.name} of ${playerCard1.suit} and ${playerCard2.name} of ${playerCard2.suit}`
-  );
-  var computerCard1 = shuffledDeck.pop();
-  var computerCard2 = shuffledDeck.pop();
-  console.log(
-    `computer cards: ${computerCard1.name} of ${computerCard1.suit} and ${computerCard2.name} of ${computerCard2.suit}`
-  );
-  // User clicks Submit to deal cards
-  if (input.length == 0) {
     // The cards are displayed to the user.
-    var message = `Player hand: ${playerCard1.name} of ${playerCard1.suit} and ${playerCard2.name} of ${playerCard2.suit} <br><br>
-  Computer hand: ${computerCard1.name} of ${computerCard1.suit} and ${computerCard2.name} of ${computerCard2.suit}`;
-
     var playerTotalHand = playerCard1.rank + playerCard2.rank;
     var computerTotalHand = computerCard1.rank + computerCard2.rank;
     console.log(`player total hand: ${playerTotalHand}`);
     console.log(`computer total hand: ${computerTotalHand}`);
 
-    // The cards are analysed for game winning conditions, e.g. Blackjack.
-    // A tie. When both the player and dealer have the same total hand values - or if both draw Blackjack
-    if (playerTotalHand == computerTotalHand) {
-      message += `<br><br> It's a tie!`;
-    }
-    // A Blackjack win. When either player or dealer draw Blackjack.
-    else if (playerTotalHand == 21) {
-      message += `<br><br> This is a Blackjack win. Player wins by black jack!`;
-    } else if (computerTotalHand == 21) {
-      message += `<br><br> This is a Blackjack win. Computer wins by black jack!`;
-    }
-    // A normal win. When neither draw Blackjack, the winner is decided by whomever has the higher hand total.
-    else if (playerTotalHand > computerTotalHand) {
-      message += `<br><br> This is a normal win. Player wins by having a higher hand total.`;
-    } else if (playerTotalHand < computerTotalHand) {
-      message += `<br><br> This is a normal win. Computer wins by having a higher hand total.`;
-    }
+    var message = `Player hand: ${playerCard1.name} of ${playerCard1.suit} and ${playerCard2.name} of ${playerCard2.suit} <br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit} and ${computerCard2.name} of ${computerCard2.suit}<br><br>
+    Your current score is ${playerTotalHand}. Do you want to hit or stand? Type h for hit or s for stand.`;
+
+    gameMode = `hitting or standing`;
     return message;
   }
+
+  // The player hitting or standing is a new mode in the game that allows the player to enter their choice.
+  if (gameMode == `hitting or standing` && input.length == 0) {
+    var playerTotalHand = playerCard1.rank + playerCard2.rank;
+    message = `Please submit h or s to proceed with game.
+    <br><br>Player hand: ${playerCard1.name} of ${playerCard1.suit} and ${playerCard2.name} of ${playerCard2.suit} <br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit} and ${computerCard2.name} of ${computerCard2.suit}<br><br>
+    Your current score is ${playerTotalHand}.`;
+  } else if (gameMode == `hitting or standing` && input == `s`) {
+    // check for double aces
+    checkDoubleAces();
+    var playerTotalHand = playerCard1.rank + playerCard2.rank;
+    var message = `Player hand: ${playerCard1.name} of ${playerCard1.suit} and ${playerCard2.name} of ${playerCard2.suit}`;
+
+    var computerTotalHand = computerCard1.rank + computerCard2.rank;
+
+    if (computerTotalHand == 17 || computerTotalHand > 17) {
+      computerTotalHand = computerCard1.rank + computerCard2.rank;
+      message += `<br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit} and ${computerCard2.name} of ${computerCard2.suit}`;
+    } else {
+      var computerCardHit = drawCard(computerCardHit);
+      computerTotalHand =
+        computerCard1.rank + computerCard2.rank + computerCardHit.rank;
+      message += `<br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit},<br>
+    ${computerCard2.name} of ${computerCard2.suit},<br>
+    ${computerCardHit.name} of ${computerCardHit.suit}`;
+    }
+
+    message += `<br><br>You chose to stand`;
+    // Your score: ${playerTotalHand}<br>
+    // Computer score: ${computerTotalHand}`;
+    gameMode = `evaluate winner`;
+  } else if (gameMode == `hitting or standing` && input == `h`) {
+    var playerCardHit = drawCard(playerCardHit);
+
+    var playerTotalHand =
+      playerCard1.rank + playerCard2.rank + playerCardHit.rank;
+    var message = `Player hand: ${playerCard1.name} of ${playerCard1.suit},<br>
+    ${playerCard2.name} of ${playerCard2.suit} and<br>
+    ${playerCardHit.name} of ${playerCardHit.suit}`;
+
+    var computerTotalHand = computerCard1.rank + computerCard2.rank;
+
+    if (computerTotalHand == 17 || computerTotalHand > 17) {
+      computerTotalHand = computerCard1.rank + computerCard2.rank;
+      message += `<br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit},<br>
+    ${computerCard2.name} of ${computerCard2.suit}`;
+    } else if (computerTotalHand < 17) {
+      var computerCardHit = drawCard(computerCardHit);
+
+      // check for double aces
+      checkDoubleAces();
+
+      computerTotalHand =
+        computerCard1.rank + computerCard2.rank + computerCardHit.rank;
+      message += `<br><br>
+    Computer hand: ${computerCard1.name} of ${computerCard1.suit},<br>
+    ${computerCard2.name} of ${computerCard2.suit},<br>
+    ${computerCardHit.name} of ${computerCardHit.suit}`;
+    }
+
+    console.log(`player total hand becomes: ${playerTotalHand}`);
+    console.log(`computer total hand: ${computerTotalHand}`);
+
+    gameMode = `evaluate winner`;
+  }
+  // evaluating winner
+  if (gameMode == `evaluate winner`) {
+    var winningImage = `<img src="https://c.tenor.com/Sjbio2e2HMUAAAAC/bts-supreme.gif"/>`;
+    // A Blackjack win. When either player or dealer draw Blackjack.
+    if (playerTotalHand == 21) {
+      gameMode = `draw card`;
+      message +=
+        `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}
+      <br><br> This is a Blackjack win. Player wins by black jack!` +
+        winningImage +
+        `<br><br> Press the submit button again to start a new round.`;
+      return message;
+    } else if (computerTotalHand == 21) {
+      gameMode = `draw card`;
+      message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}
+      <br><br> This is a Blackjack win. Computer wins by black jack!
+      <br><br> Press the submit button again to start a new round.`;
+      return message;
+    } else if (playerTotalHand < 21) {
+      if (playerTotalHand == computerTotalHand) {
+        message += `<br><br> It's a tie!
+        Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}`;
+      } else if (
+        playerTotalHand > computerTotalHand &&
+        computerTotalHand < 21
+      ) {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}<br><br> This is a normal win. Player wins by having a higher hand total.`;
+      } else if (
+        playerTotalHand < computerTotalHand &&
+        computerTotalHand < 21
+      ) {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}<br><br> This is a normal win. Computer wins by having a higher hand total.`;
+      } else {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}<br><br> You won! Computer loses by having a score higher than 21.`;
+      }
+    }
+
+    // Second Version: when the player busts (has a total score of >21)
+    if (playerTotalHand > 21) {
+      // tie scenario
+      if (playerTotalHand == computerTotalHand) {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}
+      <br><br> It's a tie!`;
+      } else if (playerTotalHand < computerTotalHand) {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}
+      <br><br> Player wins by having a score closer to 21.`;
+      }
+      // losing scenario
+      else if (playerTotalHand > computerTotalHand) {
+        message += `<br><br>Your score: ${playerTotalHand}<br>
+      Computer score: ${computerTotalHand}
+      <br><br> Player loses by having a score higher than 21.`;
+      }
+    }
+    gameMode = `draw card`;
+    message += `<br><br> Press the submit button again to start a new round.`;
+  }
+  return message;
 };
 
-// The user decides whether to hit or stand, using the submit button to submit their choice.
-
-// The user's cards are analysed for winning or losing conditions.
-
-// The computer decides to hit or stand automatically based on game rules.
-
-// The game either ends or continues.
+var checkDoubleAces = function () {
+  // reassignment of rank for player cards
+  if (playerCard1.name == `ace` && playerCard2.name == `ace`) {
+    playerCard1.rank = 11;
+    playerCard2.rank = 1;
+    console.log(`playerCard1.rank has been changed to 11`);
+  } else if (playerCard2.name == `ace` && playerCardHit.name == `ace`) {
+    playerCard2.rank = 11;
+    playerCardHit.rank = 1;
+    console.log(`playerCard2.rank has been changed to 11`);
+  } else if (playerCard1.name == `ace` && playerCardHit.name == `ace`) {
+    playerCard1.rank = 11;
+    playerCardHit.rank = 1;
+    console.log(
+      `card 1 and cardHit are both aces. playerCard1.rank has been changed to 11`
+    );
+  }
+  // reassignment of rank for computer cards
+  if (computerCard1.name == `ace` && playerCard2.name == `ace`) {
+    computerCard1.rank = 11;
+    computerCard2.rank = 1;
+    console.log(`computerCard1.rank has been changed to 11`);
+  } else if (computerCard2.name == `ace` && computerCardHit.name == `ace`) {
+    computerCard2.rank = 11;
+    computerCardHit.rank = 1;
+    console.log(`computerCard2.rank has been changed to 11`);
+  } else if (computerCard1.name == `ace` && computerCardHit.name == `ace`) {
+    computerCard1.rank = 11;
+    computerCardHit = 1;
+    console.log(
+      `card 1 and cardHit are both aces. computerCard1.rank has been changed to 11`
+    );
+  }
+};
+var drawCard = function () {
+  return shuffledDeck.pop();
+};
 
 var makeDeck = function () {
   // Initialise an empty deck array
   var cardDeck = [];
   // Initialise an array of the 4 suits in our deck, we will loop over this array
-  var suits = [`hearts`, `diamonds`, `clubs`, `spades`];
+  var suits = [`♥️`, `♦️`, `♣️`, `♠️`];
 
   // Loop over the suits array
   for (suitIndex = 0; suitIndex < suits.length; suitIndex++) {
